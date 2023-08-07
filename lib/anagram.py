b93ffe3252813478327e2daa8b3975317c57809d
Motty-har <harligmotty@gmail.com>
2023-08-06 22:07:09 -0400

    Finished

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word:
+                match_word_list.append(word)
+
+        return match_word_list
